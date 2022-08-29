df101033e59af7ab0ee116e9507c8222d497a60b
trevortinker <trevor.tinker@gmail.com>
2022-08-29 07:38:20 -0700

    latest class projects code

--- object_oriented/es6.js
+++ object_oriented/es6.js
@@ -1,21 +1,64 @@
+// class Person {
+//   constructor(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.birthday = new Date(dob);
+//   }
+
+//   greeting() {
+//     return `Hello there, ${this.firstName} ${this.lastName}!`;
+//   }
+
+//   calculateAge() {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
+
+//   getsMarried(newLastName) {
+//     this.lastName = newLastName;
+//   }
+
+//   static addNumbers(x, y) {
+//     return x + y;
+//   }
+// }
+
+// const mary = new Person('Mary', 'James', '11-13-1980');
+// mary.getsMarried('Smith');
+// console.log(mary.calculateAge());
+// console.table(mary);
+
+// console.log(Person.addNumbers(1, 3));
+
+/* --- Inheritance / Subclasses --- */
+
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
   }
 
   greeting() {
-    return `Hello there, ${this.firstName} ${this.lastName}!`;
-  }
-
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+    return `Hello there, ${this.firstName} ${this.lastName}`;
   }
 }
 
-const mary = new Person('Mary', 'James');
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
 
-console.log(mary.greeting());
+    this.phone = phone;
+    this.membership = membership;
+  }
+
+  static getMembershipCost() {
+    return 500;
+  }
+}
+
+const john = new Customer('John', 'Williams', '206-555-1212', 'Gold');
+
+console.table(john);
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
