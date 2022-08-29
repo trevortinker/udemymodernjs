d7206a16161731bed3555ae70c3cf35056c8a78e
Trevor Tinker <trevor.tinker@gmail.com>
2022-08-29 14:55:30 -0700

    add local storage to book_list

--- book_list/appes6.js
+++ book_list/appes6.js
@@ -57,6 +57,49 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function (book) {
+      const ui = new UI();
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn, index) {
+    const books = Store.getBooks();
+
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener for Add Book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   const title = document.getElementById('title').value,
@@ -76,8 +119,13 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   } else {
     // Add book to list
     ui.addBookToList(book);
+
+    // Add to Local Storage
+    Store.addBook(book);
+
     // Show success
     ui.showAlert('Book added', 'success');
+
     // Clear fields
     ui.clearFields();
   }
@@ -92,6 +140,9 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
   // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove from Local Storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show Alert
   ui.showAlert('Book removed', 'success');
